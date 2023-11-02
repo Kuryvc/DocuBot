822b4ee8b3e90a65ee2963db3aa406eab1436c2e
Andre.betancourt <andre.betancourt@toshibagcs.com>
2023-11-02 00:06:57 -0600

    Login/register almost finished

--- app.py
+++ app.py
@@ -7,54 +7,8 @@ from scripts.pdf_processing import get_pdf_text, get_text_chunks
 from scripts.vectorization import get_vector_index
 from scripts.conversation import get_conversation_chain, handle_question
 from htmlTemplate import css, bot_template, user_template
+from scripts.login import sidebar_login
 
-# Placeholder para estructura de datos
-users_data = {
-    'username': {   # Nombre del usuario
-        'password': 'hashed_password',  # Contraseña del usuario (encriptada)
-        'searches': {  # Historial de búsquedas del usuario
-            'search_name': {   # Nombre o identificador de una búsqueda específica
-                'text_chunks': [],  # Fragmentos de texto relacionados con esa búsqueda
-                'vector_store': None  # Información vectorizada (por ejemplo, para búsquedas rápidas o análisis)
-            }
-        }
-    }
-}
-def register(username, password):
-    if username not in users_data:
-        # Asegúrate de hashear la contraseña antes de guardarla
-        hashed_password = password  # Deberías cambiar esto por una función de hashing real
-        users_data[username] = {'password': hashed_password, 'searches': {}}
-        st.session_state.authenticated = True
-        st.session_state.username = username
-        st.session_state.chat_history = []
-        return True
-    else:
-        st.warning("El usuario ya existe.")
-        return False
-
-def login(username, password):
-    user_data = users_data.get(username, {})
-    hashed_password = password  # Cambia esto por una función de hashing real
-    if user_data and user_data['password'] == hashed_password:
-        st.session_state.authenticated = True
-        st.session_state.username = username
-        st.session_state.chat_history = []
-        return True
-    else:
-        st.warning("Usuario o contraseña incorrecta.")
-        return False
-
-def sidebar_login():
-    st.sidebar.header("Inicio de sesión/Registro")
-    username = st.sidebar.text_input("Usuario")
-    password = st.sidebar.text_input("Contraseña", type="password")
-
-    if st.sidebar.button("Login"):
-        login(username, password)
-
-    if st.sidebar.button("Registrar"):
-        register(username, password)
         
 def main():
     load_dotenv()
---
+++ scripts/login.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import json
+import bcrypt
+
+def hash_password(password):
+    # Genera una sal (salt) aleatoria para aumentar la seguridad
+    salt = bcrypt.gensalt()
+    
+    # Hashea la contraseña utilizando la sal
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    
+    return hashed_password
+
+
+def check_password(entered_password, hashed_password):
+    if isinstance(entered_password, str):
+        entered_password = entered_password.encode('utf-8')  # Codifica la cadena a bytes
+
+    return bcrypt.checkpw(entered_password, hashed_password)
+
+
+def register(username, password):
+    with open('user_data.json', 'r') as user_data_file:
+        user_data = json.load(user_data_file)
+
+    if username not in user_data:
+        # Asegúrate de hashear la contraseña antes de guardarla
+        hashed_password = hash_password(password)  # Utiliza tu función hash_password
+        user_data[username] = {'password': hashed_password.decode('utf-8'), 'searches': {}}
+
+        with open('user_data.json', 'w') as user_data_file:
+            json.dump(user_data, user_data_file)
+        
+        st.session_state.authenticated = True
+        st.session_state.username = username
+        st.session_state.chat_history = []
+        return True
+    else:
+        st.warning("El usuario ya existe.")
+        return False
+
+
+def login(username, password):
+    with open('user_data.json', 'r') as user_data_file:
+        user_data = json.load(user_data_file)
+
+    user_info = user_data.get(username, {})
+    stored_password_hash = user_info.get('password', '').encode('utf-8')  # Convierte a bytes
+
+    if user_info and check_password(password, stored_password_hash):
+        st.session_state.authenticated = True
+        st.session_state.username = username
+        st.session_state.chat_history = []
+        return True
+    else:
+        st.warning("Usuario o contraseña incorrecta.")
+        return False
+
+
+
+def sidebar_login():
+    st.sidebar.header("Inicio de sesión/Registro")
+    username = st.sidebar.text_input("Usuario")
+    password = st.sidebar.text_input("Contraseña", type="password")
+
+    if st.sidebar.button("Login"):
+        login(username, password)
+
+    if st.sidebar.button("Registrar"):
+        register(username, password)
