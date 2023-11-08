efbd40bc1c6fef73a32ce2699d511f811fd45bcf
kury v <ii727571@iteso.mx>
2023-11-08 09:43:11 -0600

    Dont push credentials

--- scripts/db.py
+++ scripts/db.py
@@ -5,8 +5,8 @@ def dynamo_connection():
     dynamodb = boto3.client(
         "dynamodb",
         region_name="us-east-1",
-        aws_access_key_id= "AKIAZ7XICEOYVZP5MCWF",
-        aws_secret_access_key= "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",
+        aws_access_key_id= os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key= os.getenv("AWS_SECRET_ACCESS_KEY"),
     )
 
     return dynamodb
