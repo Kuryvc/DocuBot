b82ad776efdbb84c3143b8bca645c8f775d4210b
kury v <ii727571@iteso.mx>
2023-11-08 20:16:36 -0600

    new keys not pushed

--- scripts/db.py
+++ scripts/db.py
@@ -1,12 +1,14 @@
 import boto3
 import os
+from dotenv import load_dotenv
 
 def dynamo_connection():
+    load_dotenv()
     dynamodb = boto3.client(
         "dynamodb",
         region_name="us-east-1",
         aws_access_key_id= os.getenv("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key= os.getenv("AWS_SECRET_ACCESS_KEY"),
+        aws_secret_access_key= os.getenv("AWS_SECRET_ACCESS_KEY")
     )
 
     return dynamodb
